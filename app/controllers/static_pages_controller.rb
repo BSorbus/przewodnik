e1ef7dbea5e87f477b0a2d2609209b934263c055
BSorbus <BSorbus@gmail.com>
2019-09-19 07:33:48 +0200

    Add params[:run]

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,11 @@ class StaticPagesController < ApplicationController
 
   caches_page :home, gzip: :best_speed
 
+  respond_to :html
+
   def home
+    show_extend = params[:run] == 'bi'
+    render :home, locals: { show_extend: show_extend } 
   end
 
 end
